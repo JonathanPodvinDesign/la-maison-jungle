841173f9917a30010bba16156c2c0edd5b2aebf3
JonathanPodvinDesign <jonathanpodvindesign@gmail.com>
2023-03-05 20:55:31 +0100

    Shopping list ajout plants

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { plantList } from "../datas/plantList";
+import "../styles/ShoppingList.css";
 
 const ShoppingList = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {plantList.map((plant) => (
+          <li className="lmj-plant-list" key={plant.id}>
+            {plant.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ShoppingList;
